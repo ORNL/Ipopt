af602695dfe69dee50fdf6c7534d01a8c1d13dc7
Tamar DeWilde <tamard19@gmail.com>
2026-07-24 02:43:08 -0700

    Remove unsupported ReSolve KLU options

--- src/Algorithm/LinearSolvers/IpReSolveSolverInterface.cpp
+++ src/Algorithm/LinearSolvers/IpReSolveSolverInterface.cpp
@@ -149,19 +149,6 @@ void ReSolveSolverInterface::RegisterOptions(SmartPtr<RegisteredOptions> roption
                              "ordering if P and Q are NULL), or 3 for the user order function.",
                              false);
 
-  roptions->AddIntegerOption("resolve_btf",                                                                                //
-                             "Use BTF",                                                                                    //
-                             1,                                                                                            //
-                             "if nonzero, then BTF is used to permute the input matrix into block upper triangular form.", //
-                             false);
-
-  roptions->AddIntegerOption("resolve_scale",                              //
-                             "Whether or not the matrix should be scaled", //
-                             2,                                            //
-                             "If scale < 0, then no scaling is performed and the input matrix is not checked for errors. If scale >= 0, the input matrix is check for errors. If scale=0, then no scaling is performed. If scale=1, then each row of A is "
-                             "divided by the sum of the absolute values in that row. If scale=2, then each row of A is divided by the maximum absolute value in that row. Default: 2.", //
-                             false);
-
   roptions->AddBoolOption("resolve_halt_if_singular",        //
                           "how to handle a singular matrix", //
                           false,                             // Default is False in ReSolve
@@ -192,12 +179,6 @@ bool ReSolveSolverInterface::InitializeImpl(const OptionsList& options, const st
   options.GetNumericValue("resolve_tol", pivot_tol_, prefix);
 
   options.GetIntegerValue("resolve_ordering", ordering_, prefix);
-
-  Index btf;
-  options.GetIntegerValue("resolve_btf", btf, prefix);
-
-  Index scale;
-  options.GetIntegerValue("resolve_scale", scale, prefix);
 
   Index n_skip_refactoring;
   options.GetIntegerValue("resolve_n_skip_refactoring", n_skip_refactoring, prefix);
